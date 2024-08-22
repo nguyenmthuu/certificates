c1ca95fc783a5a8d45e19d491190651bdc728a68
nguyenn-04 <hatrongnguyen0423@gmail.com>
2024-08-22 14:04:57 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import gspread
@@ -5,16 +6,30 @@ from jinja2 import Template
 from oauth2client.service_account import ServiceAccountCredentials
 
 # Thiết lập Google Sheets API
+
+# Đọc thông tin xác thực từ secret
+credentials_info = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+
+# Xác định phạm vi truy cập
 scope = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/drive",
 ]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
-client = gspread.authorize(creds)
 
-# Kết nối với Google Sheets
-sheet = client.open("My Certificates").sheet1
+# Xác thực và khởi tạo client
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_info, scope)
+client = gspread.authorize(credentials)
+
+# Mở Google Sheets bằng URL hoặc tên
+spreadsheet = client.open("My Certificates")
+sheet = spreadsheet.sheet1  # Mở sheet đầu tiên
+
+# Đọc dữ liệu từ sheet
 data = sheet.get_all_records()
+
+# Đảo ngược thứ tự dữ liệu để hiển thị từ mới nhất lên đầu
+data.reverse()
+
 
 # Đọc template HTML và render với dữ liệu mới
 with open("templates/index.html") as file_:
